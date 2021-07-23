f3b532be8a398c3be738c8c0acba96d833b54132
alefagundes <alefagundes2000.af@gmail.com>
2021-07-22 21:22:39 -0300

    Implemeting possible moves of king

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -22,13 +22,13 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Taget: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
-				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			
